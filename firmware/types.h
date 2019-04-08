91ea813a6dbb437fff0a456edd75fd02bd2c6a86
iLambda <me@ilambda.io>
2019-04-08 17:04:02 +0200

    Added keymap, keydata, and MIDImsg packed structs

--- firmware/types.h
+++ firmware/types.h
@@ -11,8 +11,14 @@
 #define I2C_LCD_ADDRESS                   0x72
 #define I2C_EXTERNAL_EEPROM_ADDRESS       0xA0
 
+/*********************************
+ *                               *
+ *    PACKED DATA STRUCTURES     *
+ *                               *
+ *********************************/
+
 /*
- * PACKED DATA STRUCTURES
+ * COLOR
  */
 
 /* A 3-byte color */
@@ -27,19 +33,96 @@ typedef union __attribute__((packed)) {
   };
 } color_t;
 
-inline bool colorEquals(const color_t &a, const color_t &b) {
-  return a.r == b.r && a.g == b.g && a.b == b.b;
-}
+/*
+ * MIDI DATA
+ */
+
+/* A midi message */
+typedef struct __attribute__((packed)) {
+  /* Command */
+  union {
+    /* The status byte as is */
+    uint8_t status;
+    /* The command/channel */
+    struct {
+      /* The channel */
+      uint8_t channel : 4;
+      /* The command */
+      uint8_t command : 4;
+    };
+  };
+  /* The data */
+  union {
+    /* The data */
+    uint16_t data;
+    /* The 2 bytes of data */
+    struct {
+      /* The lower half of data */
+      uint8_t data1;
+      /* The higher half of data */
+      uint8_t data2;
+    };
+  };
+} MIDImsg_t;
 
 /*
- * UNALIGNED DATA STRUCTURES
+ * GLOBAL SETTINGS
  */
+typedef struct __attribute__((packed)) {
+  /* Allocate 64 unused bytes */
+  uint64_t unused;
+} settings_t;
+
+/*
+ * KEYMAPS
+ */
+
+/* The mode of a key */
+typedef enum {
+  KEY_MODE_TOGGLE = 0,
+  KEY_MODE_INSTANT = 1
+} keymode_t;
+
+/* A key */
+typedef struct __attribute__((packed)) {
+  /* The configuration */
+  union {
+    /* The config byte */
+    uint8_t cfg;
+    /* The fields */
+    struct {
+      /* The keymode */
+      keymode_t mode : 1;
+    };
+  };
+  /* A midi message */
+  MIDImsg_t msg;
+
+} keydata_t;
+
+/* A keymap */
+typedef struct __attribute__((packed)) {
+  /* The color of the screen */
+  color_t backlight;
+  /* The keys */
+  keydata_t keys[NB_BUTTONS];
+
+} keymap_t;
+
+
+
+/*********************************
+ *                               *
+ *     MISC DATA STRUCTURES      *
+ *                               *
+ *********************************/
 
 typedef struct {
   /* The screen color */
   color_t backlight;
   /* The constrast */
   uint8_t contrast;
+
 } screendata_t;
 
 typedef struct __attribute__((packed)) {
@@ -48,6 +131,19 @@ typedef struct __attribute__((packed)) {
   /* The button */
   uint8_t pastButton : 1;
   uint8_t button : 1;
+
 } keystate_t;
+
+
+
+/*********************************
+ *                               *
+ *        HELPER FUNCTIONS       *
+ *                               *
+ *********************************/
+
+inline bool colorEquals(const color_t &a, const color_t &b) {
+  return a.r == b.r && a.g == b.g && a.b == b.b;
+}
 
 #endif
